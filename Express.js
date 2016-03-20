685c3357078bac1a69fb8c8ded0559980ba4aa65
Lian Lu <lianlu@asu.edu>
2016-03-20 18:08:15 -0500

    css on fly

--- Express.js
+++ Express.js
@@ -3,17 +3,6 @@
  */
 var express = require('express');
 var app = express();
-var path = require('path');
-var bodyparser = require('body-parser')
-//app.set('views', process.argv[3]);
-//app.set('view engine', 'jade');
-//
-//app.get('/home', function(req, res){
-//    res.render('index', {date: new Date().toDateString()})
-//}).listen(process.argv[2]);
-app.use(bodyparser.urlencoded({extended: false}));
-app.post('/form', function(req, res){
-    var recdata = req.body.str.split('').reverse().join('');
-    res.end(recdata);
-});
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
 app.listen(process.argv[2]);
