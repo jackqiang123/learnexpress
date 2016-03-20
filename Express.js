3394519a5ef33043791e420565d6a64c2beba43d
Lian Lu <lianlu@asu.edu>
2016-03-20 18:32:13 -0500

    overall

--- Express.js
+++ Express.js
@@ -3,11 +3,19 @@
  */
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
-app.get('/search', function(req, res){
-    var object = req.query;
+app.get('/books', function(req, res){
 
-    res.send(object);
+
+    var file;
+    fs.readFile(process.argv[3], function(err, data){
+        file = data;
+        var object = JSON.parse(file);
+        res.json(object);
+    });
+
+
 });
 
 app.listen(process.argv[2]);
