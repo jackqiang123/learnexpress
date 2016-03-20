44530e18eb000cce57f3b6a2e81fddf9dd1b6c6a
Lian Lu <lianlu@asu.edu>
2016-03-20 17:25:02 -0500

    jade

--- Express.js
+++ Express.js
@@ -4,5 +4,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
-app.listen(process.argv[2]);
+
+app.set('views', process.argv[3]);
+app.set('view engine', 'jade');
+
+app.get('/home', function(req, res){
+    res.render('index', {date: new Date().toDateString()})
+}).listen(process.argv[2]);
