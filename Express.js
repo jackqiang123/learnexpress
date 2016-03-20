156e65d811f78e3baf109bcd94c90d9503a41776
Lian Lu <lianlu@asu.edu>
2016-03-20 17:40:39 -0500

    oldformparser

--- Express.js
+++ Express.js
@@ -4,10 +4,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
-app.set('views', process.argv[3]);
-app.set('view engine', 'jade');
-
-app.get('/home', function(req, res){
-    res.render('index', {date: new Date().toDateString()})
-}).listen(process.argv[2]);
+var bodyparser = require('body-parser')
+//app.set('views', process.argv[3]);
+//app.set('view engine', 'jade');
+//
+//app.get('/home', function(req, res){
+//    res.render('index', {date: new Date().toDateString()})
+//}).listen(process.argv[2]);
+app.use(bodyparser.urlencoded({extended: false}));
+app.post('/form', function(req, res){
+    var recdata = req.body.str.split('').reverse().join('');
+    res.end(recdata);
+});
+app.listen(process.argv[2]);
